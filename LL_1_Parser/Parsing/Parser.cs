7e82e3ff1875ccd132fc20b1ba32afaac88e87aa
MarekOchocki <marcopolo97@vp.pl>
2019-06-27 03:53:07 +0200

    file writing and reading

--- LL_1_Parser/Parsing/Parser.cs
+++ LL_1_Parser/Parsing/Parser.cs
@@ -1,27 +1,36 @@
 ﻿using LL_1_Parser.Core;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace LL_1_Parser.Parsing
 {
     class Parser
     {
-        public static void Parse(ParsingTable table, Grammar grammar, string word)
+        public static void Parse(ParsingTable table, Grammar grammar, string word, StreamWriter log)
         {
             var symbols = new List<Symbol>();
             symbols.Add(grammar.startingSymbol);
 
             while(true)
             {
+                log.Write("[" + word + ", " + symbolListToString(symbols) + "$, ");
                 Console.Write("[" + word + ", " + symbolListToString(symbols) + "$, ");
 
-                if(symbols.Count == 0)
+
+                if (symbols.Count == 0)
                 {
                     if(word.Length == 0)
+                    {
+                        log.WriteLine("acc]");
                         Console.WriteLine("acc]");
+                    }
                     else
+                    {
+                        log.WriteLine("error]");
                         Console.WriteLine("error]");
+                    }
                     return;
                 }
 
@@ -29,12 +38,14 @@ namespace LL_1_Parser.Parsing
                 {
                     if(word.Length == 0)
                     {
+                        log.WriteLine("error]");
                         Console.WriteLine("error]");
                         return;
                     }
 
                     if(word[0] == symbols[0].rep)
                     {
+                        log.WriteLine("pop " + word[0] + "]");
                         Console.WriteLine("pop " + word[0] + "]");
                         symbols.RemoveAt(0);
                         word = word.Remove(0, 1);
@@ -51,10 +62,12 @@ namespace LL_1_Parser.Parsing
 
                     if(production == null)
                     {
+                        log.WriteLine("error]");
                         Console.WriteLine("error]");
                         return;
                     }
 
+                    log.WriteLine(prodNumber.ToString() + "]");
                     Console.WriteLine(prodNumber.ToString() + "]");
 
                     symbols.RemoveAt(0);
--- LL_1_Parser/Program.cs
+++ LL_1_Parser/Program.cs
@@ -1,6 +1,7 @@
 using LL_1_Parser.Core;
 using LL_1_Parser.Parsing;
 using System;
+using System.IO;
 
 namespace LL_1_Parser
 {
@@ -8,22 +9,33 @@ namespace LL_1_Parser
     {
         static void Main(string[] args)
         {
-            Grammar g = new Grammar(new Symbol('E'));
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: parser.exe <inputFile>, <outputFile> <word>");
+                Console.WriteLine("Input File format:");
+                Console.WriteLine("Line 1: Starting Symbol");
+                Console.WriteLine("Line 2: Nonterminal symbols (e.g. \"EVPFLG\"");
+                Console.WriteLine("Line 3: terminal symbols (e.g. \"+-*a()e\"), with 'e' that means epsilon");
+                Console.WriteLine("Line 4: number of productions");
+                Console.WriteLine("Line 5+: production (e.g. \"E->VP\" or \"P->+V\"");
+                return;
+            }
 
-            g.AddNonTerminalSymbols("EVPFLG");
-            g.AddTerminalSymbols("+-*a()e");
 
-            g.productions.Add(Production.FromString("E->VP"));
-            g.productions.Add(Production.FromString("P->+V"));
-            g.productions.Add(Production.FromString("P->e"));
-            g.productions.Add(Production.FromString("V->-V"));
-            g.productions.Add(Production.FromString("V->F"));
-            g.productions.Add(Production.FromString("F->aLG"));
-            g.productions.Add(Production.FromString("F->(E)G"));
-            g.productions.Add(Production.FromString("L->(E)"));
-            g.productions.Add(Production.FromString("L->e"));
-            g.productions.Add(Production.FromString("G->*G"));
-            g.productions.Add(Production.FromString("G->e"));
+            StreamReader sr = new StreamReader(args[0]);
+            StreamWriter wr = new StreamWriter(args[1]);
+
+            Grammar g = new Grammar(new Symbol(sr.ReadLine()[0]));
+
+            g.AddNonTerminalSymbols(sr.ReadLine());
+            g.AddTerminalSymbols(sr.ReadLine());
+
+            int howManyProductions = int.Parse(sr.ReadLine());
+            
+            for(int i = 0; i < howManyProductions; i++)
+            {
+                g.productions.Add(Production.FromString(sr.ReadLine()));
+            }
 
             ParsingTable parsingTable = new ParsingTable(g);
 
@@ -34,7 +46,17 @@ namespace LL_1_Parser
             Console.WriteLine(parsingTable.ToString());
             Console.WriteLine();
 
-            Parser.Parse(parsingTable, g, "(-a+a(a*))+a");
+            wr.WriteLine(g.ToString());
+            wr.WriteLine();
+            wr.WriteLine(parsingTable.ProductionsInfo());
+            wr.WriteLine();
+            wr.WriteLine(parsingTable.ToString());
+            wr.WriteLine();
+
+            Parser.Parse(parsingTable, g, args[2], wr);
+
+            sr.Close();
+            wr.Close();
 
             Console.ReadKey();
             
